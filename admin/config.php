7b61405351613defe601c61fc4b01a8b78bb9f2d
Humaun Kabir <asmhumaunkabir@gmail.com>
2021-07-13 14:18:23 +0600

    now i can add user and admin

---
+++ admin/config.php
@@ -0,0 +1,4 @@
+<?php
+    $conn = mysqli_connect("localhost","root","","news-site") or die("Connection failed" . mysqli_connet_error());
+
+?>
---
+++ footer.php
@@ -0,0 +1,11 @@
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>www.asmhumaunkabir.com</span>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>

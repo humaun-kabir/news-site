acbd6afcb10de41bb7793483923f654a17a2f091
Humaun Kabir <asmhumaunkabir@gmail.com>
2021-07-16 16:25:48 +0600

    delete image from folder

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -4,6 +4,14 @@
     $post_id = $_GET['id'];
     $cat_id = $_GET['catid'];
 
+    $sql1 = "SELECT * FROM post WHERE post_id = {$post_id}";
+    $result = mysqli_query($conn, $sql1) or die("Query failed : Selecct");
+    $row = mysqli_fetch_assoc($result);
+
+    unlink("upload/".$row['post_image']);
+
+
+
     $sql = "DELETE FROM post WHERE post_id = {$post_id};";
 
     $sql .= "UPDATE category SET post = post - 1 WHERE category_id = {$cat_id}";
